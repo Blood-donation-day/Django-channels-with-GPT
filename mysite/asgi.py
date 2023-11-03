8532e9301641e0e7985bd83d972caa7c5a00cf58
[KIMCHANGHWAN] <98susckdghks@naver.com>
2023-11-03 17:11:31 +0900

    웹소켓 요청에서 RolePlayingRoom 인스턴스 조회

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,6 +1,7 @@
 import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
@@ -11,10 +12,14 @@ django_asgi_app = get_asgi_application()
 # 🔥 HERE
 import chat.routing  # noqa
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # 🔥 HERE
-    "websocket": URLRouter(
-        chat.routing.websocket_urlpatterns
-    ),
-})
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        #채널스 기본에서는 쿠키/세션/인증을 비활성화 // self.scope에서 접근가능
+        #self.scope["user"].is_authenticated 에서 인증여부 확인
+        "websocket": AuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns),
+        ),
+    }
+)
